295f583d044287386804ee4fee2f9e5e31dd7318
aslakamsani <amit_lakamsani@yahoo.com>
2014-01-18 19:38:44 -0800

    Waypoints
    Waypoints now work. Radar is implemented. Images need to be changed.

--- src/com/ploigos/MainActivity.java
+++ src/com/ploigos/MainActivity.java
@@ -1,15 +1,37 @@
 package com.ploigos;
 
-import android.os.Bundle;
+import java.io.IOException;
+
 import android.app.Activity;
+import android.content.Intent;
+import android.os.AsyncTask;
+import android.os.Bundle;
+import android.util.Log;
 import android.view.Menu;
+//import android.view.View;
+//import android.webkit.WebSettings;
+
+//import com.metaio.Example.BuildConfig;
+//import com.metaio.Example.MainActivity.WebViewHandler;
+import com.metaio.sdk.MetaioDebug;
+import com.metaio.tools.io.AssetsManager;
 
 public class MainActivity extends Activity {
 
+	AssetsExtracter task;
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		MetaioDebug.enableLogging(BuildConfig.DEBUG);
+		
+		task = new AssetsExtracter();
+		task.execute(0);
+		
+		Intent intent = new Intent(getApplicationContext(), ARActivity.class);
+		startActivity(intent);
 	}
 
 	@Override
@@ -19,4 +41,27 @@ public class MainActivity extends Activity {
 		return true;
 	}
 
+	/**
+	 * This task extracts all the assets to an external or internal location
+	 * to make them accessible to metaio SDK
+	 */
+	private class AssetsExtracter extends AsyncTask<Integer, Void, Boolean>
+	{		
+		@Override
+		protected Boolean doInBackground(Integer... params) 
+		{
+			try 
+			{
+				// Extract all assets and overwrite existing files if debug build
+				AssetsManager.extractAllAssets(getApplicationContext(), true);
+			} 
+			catch (IOException e) 
+			{
+				MetaioDebug.printStackTrace(Log.ERROR, e);
+				return false;
+			}
+
+			return true;
+		}
+	}
 }
